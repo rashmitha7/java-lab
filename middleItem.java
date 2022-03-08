82d432a250f2eac23acffebd7d8fc65ef170218b
rashmitha7 <52447027+rashmitha7@users.noreply.github.com>
2022-03-08 10:48:08 +0530

    Update middleItem.java

--- middleItem.java
+++ middleItem.java
@@ -2,28 +2,23 @@ import java.util.Scanner;
 
 public class middleItem {
    public static void main(String[] args) {
-      Scanner scnr = new Scanner(System.in);
-      int[] arr = new int[9];     
-
-      int numValues;
-      int curValue;
-
-      numValues = 0;
-      curValue = scnr.nextInt();
-
-      while (curValue != -1) {
-         if (numValues == 9) {
-            System.out.println("Too many inputs");
-               return;
-         }
-            arr[numValues++] = curValue;
-            curValue = scnr.nextInt();
-         }
-         if (numValues % 2 == 1) {
-            System.out.println(arr[numValues / 2]);
-         }
-         else {
-            System.out.println(arr[(numValues - 1) / 2] + " " + arr[numValues / 2]);
-         }
+      Scanner sc = new Scanner(System.in);
+      String inp = sc.nextLine();
+      String[] tokens = inp.split(" ");
+      int[] numbers = new int[tokens.length];
+      for( int i=0;i<numbers.length;i++){
+         numbers[i]=Integer.parseInt(tokens[i]);
+      }
+      int n = numbers.length;
+      if(n>9){
+         System.out.println("Too many Outputs");
+         return;
+      }
+      if (n % 2 == 1) {
+         System.out.println(numbers[n / 2]);
+      }
+      else {
+         System.out.println(numbers[(n - 1) / 2] + " " + numbers[n / 2]);
+      }
    }
 }
